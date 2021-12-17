ea7168b4d43ba5b906e13b6e3a27021f10c29f8b
Georg Braunbeck <georg.braunbeck@impresssol.de>
2021-12-17 09:24:37 +0100

    day template update

--- src/DayTemplate.kt
+++ src/DayTemplate.kt
@@ -1,22 +1,18 @@
 import util.toDayIndex
 
+val dayIndex = 1.toDayIndex()
+
 fun main() {
-    val dayIndex = 1.toDayIndex()
-
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readTxtFile("day$dayIndex/Day${dayIndex}_test")
-    val part1Result = part1(testInput)
-    check(part1Result == 1) { "part 1 result is: $part1Result" }
-
     val input = readTxtFile("day$dayIndex/Day$dayIndex")
     println(part1(input))
     println(part2(input))
+}
+
+
+fun part1(input: List<String>): Int {
+    return input.size
+}
+
+fun part2(input: List<String>): Int {
+    return input.size
 }
